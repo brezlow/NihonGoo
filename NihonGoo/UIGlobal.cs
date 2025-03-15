aefef9f4798fbf73351b8555293e28db26fad9d4
brezlow <breezeblow@outlook.com>
2025-03-15 13:38:48 +0800

    fix: add mica

--- NihonGoo/UIGlobal.cs
+++ NihonGoo/UIGlobal.cs
@@ -3,11 +3,18 @@ using Avalonia.Controls;
 
 namespace NihonGoo;
 
-public class UIGlobal: AvaloniaObject
+internal sealed class UIGlobal : AvaloniaObject
 {
-    
+
+    // IMPORTANT: make sure you set "inherits" to true for attached property registration, otherwise style selectors will not work!
     public static readonly string TransparencyEnabled = nameof(TransparencyEnabled);
-    public static readonly AttachedProperty<bool> TransparencyEnabledProperty = AvaloniaProperty.RegisterAttached<UIGlobal, Control, bool>(nameof(TransparencyEnabled), true, true);
-    public static void SetTransparencyEnabled(Control obj, bool value) => obj.SetValue(TransparencyEnabledProperty, value);
+
+    public static readonly AttachedProperty<bool> TransparencyEnabledProperty =
+        AvaloniaProperty.RegisterAttached<UIGlobal, Control, bool>(nameof(TransparencyEnabled), true, true);
+
+    public static void SetTransparencyEnabled(Control obj, bool value) =>
+        obj.SetValue(TransparencyEnabledProperty, value);
+
     public static bool GetTransparencyEnabled(Control obj) => obj.GetValue(TransparencyEnabledProperty);
 }
+    
