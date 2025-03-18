b7e76e31f72d7a8ef4506d9c30b78cbb4d3b38de
brezlow <breezeblow@outlook.com>
2025-03-18 19:42:12 +0800

    feat: add speech synthesis for Kana characters and improve UI interaction

--- NihonGoo/ViewModels/MainWindowViewModel.cs
+++ NihonGoo/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Speech.Synthesis;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
@@ -16,6 +17,15 @@ public partial class MainWindowViewModel : ViewModelBase
     [ObservableProperty] private ObservableCollection<WindowTransparencyLevel> _transparencyLevelHint = [];
     [ObservableProperty] private ObservableCollection<Kana> _kanaList;
 
+    private readonly SpeechSynthesizer _synthesizer = new();
+
+    public void SpeakKana(Kana kana)
+    {
+        if(!kana.IsNotEmpty)return;
+        _synthesizer.SelectVoice("Microsoft Haruka Desktop");
+        _synthesizer.SpeakAsync(kana.Character+" ");
+    }
+
     public MainWindowViewModel()
     {
         _darkModeEnabled = Application.Current!.ActualThemeVariant == ThemeVariant.Dark;
@@ -30,25 +40,25 @@ public partial class MainWindowViewModel : ViewModelBase
             new("な", "na"), new("に", "ni"), new("ぬ", "nu"), new("ね", "ne"), new("の", "no"),
             new("は", "ha"), new("ひ", "hi"), new("ふ", "fu"), new("へ", "he"), new("ほ", "ho"),
             new("ま", "ma"), new("み", "mi"), new("む", "mu"), new("め", "me"), new("も", "mo"),
-            new("や", "ya"), new("ゆ", "yu"), new("よ", "yo"),
+            new("や", "ya"), new("", "", false), new("ゆ", "yu"), new("", "", false), new("よ", "yo"),
             new("ら", "ra"), new("り", "ri"), new("る", "ru"), new("れ", "re"), new("ろ", "ro"),
-            new("わ", "wa"), new("を", "wo"), new("ん", "n"), new("っ", "tsu"),
+            new("わ", "wa"), new("", "", false), new("", "", false), new("", "", false), new("を", "wo"),
+            new("ん", "n"), new("", "", false), new("", "", false), new("", "", false), new("", "", false),
             new("が", "ga"), new("ぎ", "gi"), new("ぐ", "gu"), new("げ", "ge"), new("ご", "go"),
             new("ざ", "za"), new("じ", "ji"), new("ず", "zu"), new("ぜ", "ze"), new("ぞ", "zo"),
             new("だ", "da"), new("ぢ", "ji"), new("づ", "zu"), new("で", "de"), new("ど", "do"),
             new("ば", "ba"), new("び", "bi"), new("ぶ", "bu"), new("べ", "be"), new("ぼ", "bo"),
             new("ぱ", "pa"), new("ぴ", "pi"), new("ぷ", "pu"), new("ぺ", "pe"), new("ぽ", "po"),
-            new("きゃ", "kya"), new("きゅ", "kyu"), new("きょ", "kyo"),
-            new("しゃ", "sha"), new("しゅ", "shu"), new("しょ", "sho"),
-            new("ちゃ", "cha"), new("ちゅ", "chu"), new("ちょ", "cho"),
-            new("にゃ", "nya"), new("にゅ", "nyu"), new("にょ", "nyo"),
-            new("ひゃ", "hya"), new("ひゅ", "hyu"), new("ひょ", "hyo"),
-            new("みゃ", "mya"), new("みゅ", "myu"), new("みょ", "myo"),
-            new("りゃ", "rya"), new("りゅ", "ryu"), new("りょ", "ryo"),
-            new("ぎゃ", "gya"), new("ぎゅ", "gyu"), new("ぎょ", "gyo"),
-            new("じゃ", "ja"), new("じゅ", "ju"), new("じょ", "jo"),
-            new("びゃ", "bya"), new("びゅ", "byu"), new("びょ", "byo"),
-            new("ぴゃ", "pya"), new("ぴゅ", "pyu"), new("ぴょ", "pyo"),
+            new("きゃ", "kya"), new("きゅ", "kyu"), new("きょ", "kyo"), new("", "", false), new("", "", false),
+            new("しゃ", "sha"), new("しゅ", "shu"), new("しょ", "sho"), new("", "", false), new("", "", false),
+            new("ちゃ", "cha"), new("ちゅ", "chu"), new("ちょ", "cho"), new("", "", false), new("", "", false),
+            new("にゃ", "nya"), new("にゅ", "nyu"), new("にょ", "nyo"), new("", "", false), new("", "", false),
+            new("ひゃ", "hya"), new("ひゅ", "hyu"), new("ひょ", "hyo"), new("", "", false), new("", "", false),
+            new("みゃ", "mya"), new("みゅ", "myu"), new("みょ", "myo"), new("", "", false), new("", "", false),
+            new("りゃ", "rya"), new("りゅ", "ryu"), new("りょ", "ryo"), new("", "", false), new("", "", false),
+            new("ぎゃ", "gya"), new("ぎゅ", "gyu"), new("ぎょ", "gyo"), new("", "", false), new("", "", false),
+            new("びゃ", "bya"), new("びゅ", "byu"), new("びょ", "byo"), new("", "", false), new("", "", false),
+            new("ぴゃ", "pya"), new("ぴゅ", "pyu"), new("ぴょ", "pyo"), new("", "", false), new("", "", false),
         };
     }
 
@@ -82,11 +92,13 @@ public partial class MainWindowViewModel : ViewModelBase
     {
         public string Character { get; set; }
         public string Romanization { get; set; }
+        public bool IsNotEmpty { get; set; }
 
-        public Kana(string character, string romanization)
+        public Kana(string character, string romanization, bool isNotEmpty = true)
         {
             Character = character;
             Romanization = romanization;
+            IsNotEmpty = isNotEmpty;
         }
     }
 }
--- NihonGoo/Views/MainWindow.axaml.cs
+++ NihonGoo/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using NihonGoo.ViewModels;
 
 namespace NihonGoo.Views;
 
@@ -7,5 +9,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnKanaPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is Border border && border.DataContext is MainWindowViewModel.Kana kana)
+        {
+            ((MainWindowViewModel)DataContext!).SpeakKana(kana);
+        }
     }
 }
