0207301674efe89ab5042f2b6dfb69ecbff5dcf7
brezlow <breezeblow@outlook.com>
2025-03-28 21:34:20 +0800

    feat: update FluentAvalonia integration and enhance splash screen functionality

--- NihonGoo/Views/MainWindow.axaml.cs
+++ NihonGoo/Views/MainWindow.axaml.cs
@@ -1,15 +1,51 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using FluentAvalonia.UI.Windowing;
 using NihonGoo.ViewModels;
 
 namespace NihonGoo.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : AppWindow
 {
     public MainWindow()
     {
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        SplashScreen = new MainAppSplashScreen(this);
+
         InitializeComponent();
     }
+
+
+    internal class MainAppSplashScreen : IApplicationSplashScreen
+    {
+        public MainAppSplashScreen(MainWindow owner)
+        {
+            _owner = owner;
+        }
+
+        public string AppName { get; }
+        public IImage AppIcon { get; }
+        public object SplashScreenContent { get; }
+        public int MinimumShowTime => 2000;
+
+        public Action InitApp { get; set; }
+
+        public Task RunTasks(CancellationToken cancellationToken)
+        {
+            if (InitApp == null)
+                return Task.CompletedTask;
+
+            return Task.Run(InitApp, cancellationToken);
+        }
+
+        private MainWindow _owner;
+    }
+
 
     private void OnKanaPointerPressed(object? sender, PointerPressedEventArgs e)
     {
